a7dd853ad8c65969963561cdcc068272c08f482b
Peter Molokwu <bonduu01@gmail.com>
2020-07-08 12:27:05 +0100

    Fixed project bug

--- src/main/java/com/udacity/controller/HelloWorldController.java
+++ src/main/java/com/udacity/controller/HelloWorldController.java
@@ -1,18 +1,18 @@
 package com.udacity.controller;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
 @EnableAutoConfiguration
 public class HelloWorldController
 {
-    @RequestMapping("/udacity/")
-    @ResponseBody
-    public String sayHello()
-    {
-        return "Hello Udacity, My name is Peter Molokwu and this is my fnal Captone project on Udacity's Nano program Cloud DevOps Engineer";
+    @RequestMapping(value = "/index")
+    public String index() {
+        return "index";
     }
 }
