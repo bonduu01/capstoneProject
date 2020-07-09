889b20e836c2b783b69be675145953055be936a0
Peter Molokwu <bonduu01@gmail.com>
2020-07-09 11:14:08 +0100

    Fix bug

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,14 @@
 
 docker pull bonduu01/udacitycaptoneproject:latest
 
+#docker deploy command to deploy application as a container
+
+docker run -d -p 8282:8080 -name udacitycaptoneproject bonduu01/udacitycaptoneproject:latest
+
 #Deploys image into target server into AWS server with static private ip: 172.31.6.246
-kubectl create deployment udacityCapstomeProject --image=bonduu01/udacitycaptoneproject:latest
+
+kubectl create deployment udacitycapstoneproject --image=bonduu01/udacitycaptoneproject:latest
+
+#Run deployed image on port 8080
+
+kubectl run udacitycapstoneproject --image=bonduu01/udacitycaptoneproject:latest --port=8080
